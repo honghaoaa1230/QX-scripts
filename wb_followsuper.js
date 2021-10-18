9c0fbbc8b4908e0c2a80267d94b46bf9a3775adf
honghaoaa1230 <honghaoaa1230@gmail.com>
2021-10-19 00:19:18 +0800

    title sub

--- wb_followsuper.js
+++ wb_followsuper.js
@@ -1,1 +1,10 @@
+const url = $request.url;
 let body = $response.body;
+
+if (url.indexOf("followsuper") != -1){
+    let obj = JSON.parse(body);
+    obj[cards][card_group][1].title_sub = '金多娟';
+    body = JSON.stringify(obj);
+}
+
+$done({ body });
