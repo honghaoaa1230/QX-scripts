417489839694a393963117f65dbb156126555833
honghaoaa1230 <honghaoaa1230@gmail.com>
2021-10-19 02:47:05 +0800

    final_1

--- wb_followsuper.js
+++ wb_followsuper.js
@@ -4,11 +4,14 @@ let body = $response.body;
 if (url.indexOf("followsuper") != -1){
     let obj = JSON.parse(body);
     obj["cards"][0]["card_group"][1].title_sub = "金多娟";
+    obj["cards"][0]["card_group"][1].pic = "https://wx4.sinaimg.cn/thumbnail/0088wtTqly8gvhtdlu2g2j60u00u0n0402.jpg";
+    obj["cards"][0]["card_group"][2].title_sub = "江崎光";
+    obj["cards"][0]["card_group"][2].pic = "https://wx4.sinaimg.cn/thumb180/005LFVYnly8gud16en9s5j60u00u0wiw02.jpg";
     body = JSON.stringify(obj);
     console.log('followsuper detected!');
-    $notify("followsuper!");
+    // $notify("followsuper!");
 }
-console.log('followsuper detected outside!');
-$notify("followsuperout!");
+// console.log('followsuper detected outside!');
+// $notify("followsuperout!");
 
 $done({ body });
