a729fb8a73580023325f2ea37f51b41c454ebc80
honghaoaa1230 <honghaoaa1230@gmail.com>
2021-10-19 02:37:54 +0800

    fix3

--- wb_followsuper.js
+++ wb_followsuper.js
@@ -3,7 +3,7 @@ let body = $response.body;
 
 if (url.indexOf("followsuper") != -1){
     let obj = JSON.parse(body);
-    obj[cards][0][card_group][1].title_sub = '金多娟';
+    obj["cards"][0]["card_group"][1].title_sub = "金多娟";
     body = JSON.stringify(obj);
     console.log('followsuper detected!');
     $notify("followsuper!");
